6327c0531e8712a47950ad92dc5769d2cf6803f0
theDataFox <thedatafox@outlook.com>
2018-05-18 20:14:12 +0100

    adding in basics

---
+++ 2-JS-Basics/2-JS-Basics/script.js
@@ -0,0 +1,17 @@
+job = prompt('What do you do?');
+
+switch (job){
+	case 'teacher':
+		console.log('Teaching');
+		break;
+	case 'driver':
+		console.log('Driving around')
+		break;
+	default:
+		console.log('Nothing')
+		break;
+
+
+}
+
+console.log('')
